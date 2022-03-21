982c47eeb787432fe3e9e51ddc66ecb266c2359a
PegahM <pegahh.monzeri@gmail.com>
2022-03-22 02:54:06 +0430

    Updated the GitHub link

--- src/Note.js
+++ src/Note.js
@@ -4,10 +4,8 @@ import "./Note.css";
 export default function Note() {
   return (
     <p className="note">
-      <a href="https://github.com/PegahM/Shecodes-weather-app">
-        Open-source code
-      </a>
-      , by Pegah Monzeri
+      <a href="https://github.com/PegahM/weather-react">Open-source code</a>, by
+      Pegah Monzeri
     </p>
   );
 }
